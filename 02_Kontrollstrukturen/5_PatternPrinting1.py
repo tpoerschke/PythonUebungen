359cfc2a2b1f1a37654344805b8cdbb7de67f5c2
Tim Poerschke <info@coder-tim.de>
2021-07-24 00:13:47 +0200

    Aufgabe 5 der Konstrollstrukturen

---
+++ 02_Kontrollstrukturen/5_PatternPrinting1.py
@@ -0,0 +1,15 @@
+UPPER_BOUND = 10
+
+# Kompakte Lösung:
+for i in range(1, UPPER_BOUND):
+    print(str(i) * i, end=" ")
+
+print() # Zeilenumbruch
+
+# Alternative Lösung:
+for i in range(1, UPPER_BOUND):
+    for _ in range(0, i):
+        print(i, end="")
+    print(" ", end="")
+
+print() # Zeilenumbruch
